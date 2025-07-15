936a4c4bca00d9c83956393f87a8a4f7f0fd8b08
lawrence-liu10 <88170286+lawrence-liu10@users.noreply.github.com>
2025-07-14 23:37:52 -0400

    Add command execution and file redirection

--- command.cc
+++ command.cc
@@ -16,7 +16,11 @@
 
 #include <cstdio>
 #include <cstdlib>
-
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <iostream>
 
 #include "command.hh"
@@ -31,6 +35,7 @@ Command::Command() {
     _inFile = NULL;
     _errFile = NULL;
     _background = false;
+    _append = false;
 }
 
 void Command::insertSimpleCommand( SimpleCommand * simpleCommand ) {
@@ -64,6 +69,8 @@ void Command::clear() {
     _errFile = NULL;
 
     _background = false;
+
+    _append = false;
 }
 
 void Command::print() {
@@ -105,6 +112,112 @@ void Command::execute() {
     // For every simple command fork a new process
     // Setup i/o redirection
     // and call exec
+
+    // save default in, out, and err
+    int defaultin = dup( 0 );
+	int defaultout = dup( 1 );
+	int defaulterr = dup( 2 );
+    
+    // read file input, or use stdin
+    int fdin;
+    if (_inFile) {
+        fdin = open(_inFile->c_str(), O_RDONLY);
+    }   
+    else {
+        fdin = dup(defaultin);
+    }
+
+    // create err file for RW if directing, and check if appending
+    int fderr;
+    if (_errFile) {
+        if(_append) {
+            fderr = open(_errFile->c_str(), O_WRONLY | O_CREAT | O_APPEND , 0600);
+        }
+        else {
+            fderr = open(_errFile->c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0600);
+        }
+    }
+    else {
+        fderr = dup(defaulterr);
+    }
+
+    // move 2 to point to fderr for (potential) redirection
+    dup2(fderr, 2);
+    close(fderr);
+
+    int prevPipeRead = fdin;
+    std::vector<pid_t> childPids;
+
+    for (unsigned long i = 0; i < _simpleCommands.size(); i++) {
+        
+        dup2(prevPipeRead, 0);
+        close(prevPipeRead);
+
+        // for the last simple command, check if we need to output (append) to a file
+        if (i == _simpleCommands.size() - 1) {
+            int fdout;
+            if (_outFile) {
+                if (_append) {
+                    fdout = open(_outFile->c_str(), O_WRONLY | O_CREAT | O_APPEND , 0600);
+                }
+                else {
+                    fdout = open(_outFile->c_str(), O_WRONLY | O_CREAT | O_TRUNC , 0600);
+                }
+            }
+            else {
+                fdout = dup(defaultout);
+            }
+            dup2(fdout, 1);
+            close(fdout);
+        }
+        else {
+            // keep piping to last command
+            int pipefd[2];
+            if (pipe(pipefd)) {
+                perror("pipe");
+                break;
+            }
+            dup2(pipefd[1], 1); // write back to the pipe
+            close(pipefd[1]);
+            prevPipeRead = pipefd[0]; // send pipe as input to next
+        }
+
+        //execute
+        pid_t pid = fork();
+        if (pid == 0) { //child, close the fds
+            close(defaultin);
+            close(defaultout);
+            close(defaulterr);
+
+            // read the args and execute
+            std::vector<const char *> args;
+            for (size_t j = 0; j < _simpleCommands[i]->_arguments.size(); j++) {
+                args.push_back(_simpleCommands[i]->_arguments[j]->c_str());
+            }
+            args.push_back(nullptr);
+
+            execvp(args[0], const_cast<char* const*>(args.data()));
+            exit(1);
+        }
+        else if (pid < 0) { // fork error
+            break;
+        }
+
+        childPids.push_back(pid);
+    }
+
+    dup2(defaultin, 0);
+    dup2(defaultout, 1);
+    dup2(defaulterr, 2);
+    close(defaultin);
+    close(defaultout);
+    close(defaulterr);
+
+    if (!_background) {
+        for (pid_t pid : childPids) {
+            waitpid(pid, nullptr, 0);
+        }
+    }
 
     // Clear to prepare for next command
     clear();
